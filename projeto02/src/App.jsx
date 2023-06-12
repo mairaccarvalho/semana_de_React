fabf98927ce0fc6b44941d49b85d06b7c73406de
Maira <Mairacardoso432@gmail.com>
2023-06-12 13:58:11 -0300

    Primeira semana de react

---
+++ projeto02/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <h1>olá</h1>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque, libero.</p>
+     <h2>teste h2</h2>
+     
+    </>
+  )
+}
+//
+export default App
